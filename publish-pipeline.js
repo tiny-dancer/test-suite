0e35e1ca7621ceef482945321c7191e9d1d4b0d8
Jesse Ruder <jesse@getexponent.com>
2017-02-10 22:15:37 +0000

    Add publishProjectAsync to XDL
    fbshipit-source-id: 2b95a6a48

--- publish-pipeline.js
+++ publish-pipeline.js
@@ -12,11 +12,6 @@ import JsonFile from '@exponent/json-file';
 import spawnAsync from '@exponent/spawn-async';
 import path from 'path';
 
-const XDL_LOGIN_USERPASS = {
-  username: 'exponent_ci_bot',
-  password: 'imabot2017',
-};
-
 const universePath = Constants.CODE_UNIVERSE_DIR;
 const testSuitePath = path.join(universePath, 'apps', 'test-suite');
 
@@ -34,12 +29,6 @@ export default {
     publishTestSuite(branch, tag),
   ]),
 };
-
-async function sleepAsync(timeout) {
-  return new Promise((resolve, reject) => {
-    setTimeout(resolve, timeout);
-  });
-}
 
 const publishTestSuite = (branch, tag) => ({
   name: 'Publish Test Suite',
@@ -75,28 +64,12 @@ const publishTestSuite = (branch, tag) => ({
       cwd: testSuitePath,
     });
 
-    Log.collapsed('Building XDL...');
-    const xdl = await XDL.requireAsync();
-
-    Log.collapsed('Logging in...');
-    await xdl.User.loginAsync('user-pass', XDL_LOGIN_USERPASS);
-
     Log.collapsed('Modifying slug...');
     let expJsonFile = new JsonFile(path.join(testSuitePath, 'exp.json'));
     let expJson = await expJsonFile.readAsync();
     expJson.slug = `test-suite-${process.env.UNIVERSE_BUILD_ID}`;
     await expJsonFile.writeAsync(expJson);
 
-    Log.collapsed('Starting project...');
-    await xdl.Project.startAsync(testSuitePath);
-
-    await sleepAsync(5000);
-
-    Log.collapsed('Publishing...');
-    await xdl.Project.publishAsync(testSuitePath);
-
-    Log.collapsed('Stopping project...');
-    await xdl.Project.stopAsync(testSuitePath);
-    Log.collapsed('Done...');
+    await XDL.publishProjectAsync(testSuitePath);
   },
 });
