63c3d6ccffdc5e2a01b637d3ac97d78ecce49e14
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2017-06-16 00:05:35 +0000

    `Location`: reflect error message when OS-disabled
    fbshipit-source-id: 3c31876dd

--- Tests/Location.js
+++ Tests/Location.js
@@ -8,7 +8,7 @@ import * as TestUtils from '../TestUtils';
 export const name = 'Location';
 
 export function test(t) {
-  t.describe('Location', () => {
+  t.fdescribe('Location', () => {
     t.describe('Location.getProviderStatusAsync()', () => {
       const timeout = 1000;
       t.it(
@@ -59,36 +59,47 @@ export function test(t) {
       //   7. Retry from app restart.
 
       const testShapeOrUnauthorized = options => async () => {
-        const {
-          status,
-        } = await TestUtils.acceptPermissionsAndRunCommandAsync(() => {
-          return Permissions.askAsync(Permissions.LOCATION);
-        });
-        if (status === 'granted') {
+        const providerStatus = await Location.getProviderStatusAsync();
+        if (providerStatus.locationServicesEnabled) {
           const {
-            coords: {
-              latitude,
-              longitude,
-              altitude,
-              accuracy,
-              altitudeAccuracy,
-              heading,
-              speed,
-            },
-            timestamp,
-          } = await Location.getCurrentPositionAsync(options);
-          t.expect(typeof latitude === 'number').toBe(true);
-          t.expect(typeof longitude === 'number').toBe(true);
-          t.expect(typeof altitude === 'number').toBe(true);
-          t.expect(typeof accuracy === 'number').toBe(true);
-          t
-            .expect(
-              Platform.OS !== 'ios' || typeof altitudeAccuracy === 'number'
-            )
-            .toBe(true);
-          t.expect(typeof heading === 'number').toBe(true);
-          t.expect(typeof speed === 'number').toBe(true);
-          t.expect(typeof timestamp === 'number').toBe(true);
+            status,
+          } = await TestUtils.acceptPermissionsAndRunCommandAsync(() => {
+            return Permissions.askAsync(Permissions.LOCATION);
+          });
+          if (status === 'granted') {
+            const {
+              coords: {
+                latitude,
+                longitude,
+                altitude,
+                accuracy,
+                altitudeAccuracy,
+                heading,
+                speed,
+              },
+              timestamp,
+            } = await Location.getCurrentPositionAsync(options);
+            t.expect(typeof latitude === 'number').toBe(true);
+            t.expect(typeof longitude === 'number').toBe(true);
+            t.expect(typeof altitude === 'number').toBe(true);
+            t.expect(typeof accuracy === 'number').toBe(true);
+            t
+              .expect(
+                Platform.OS !== 'ios' || typeof altitudeAccuracy === 'number'
+              )
+              .toBe(true);
+            t.expect(typeof heading === 'number').toBe(true);
+            t.expect(typeof speed === 'number').toBe(true);
+            t.expect(typeof timestamp === 'number').toBe(true);
+          } else {
+            let error;
+            try {
+              await Location.getCurrentPositionAsync(options);
+            } catch (e) {
+              error = e;
+            }
+            t.expect(error.message).toMatch(/Not authorized/);
+          }
         } else {
           let error;
           try {
@@ -96,7 +107,7 @@ export function test(t) {
           } catch (e) {
             error = e;
           }
-          t.expect(error.message).toMatch(/Not authorized/);
+          t.expect(error.message).toMatch(/Location services are disabled/);
         }
       };
 
@@ -105,26 +116,26 @@ export function test(t) {
 
       t.it(
         'gets a result of the correct shape (without high accuracy), or ' +
-          'throws error if no permission',
+          'throws error if no permission or disabled',
         testShapeOrUnauthorized({ enableHighAccuracy: false }),
         timeout
       );
       t.it(
         'gets a result of the correct shape (without high accuracy), or ' +
-          'throws error if no permission (when trying again immediately)',
+          'throws error if no permission or disabled (when trying again immediately)',
         testShapeOrUnauthorized({ enableHighAccuracy: false }),
         timeout
       );
       t.it(
         'gets a result of the correct shape (with high accuracy), or ' +
-          'throws error if no permission (when trying again immediately)',
+          'throws error if no permission or disabled (when trying again immediately)',
         testShapeOrUnauthorized({ enableHighAccuracy: true }),
         timeout
       );
 
       t.it(
         'gets a result of the correct shape (without high accuracy), or ' +
-          'throws error if no permission (when trying again after 1 second)',
+          'throws error if no permission or disabled (when trying again after 1 second)',
         async () => {
           await new Promise(resolve => setTimeout(resolve, 1000));
           await testShapeOrUnauthorized({ enableHighAccuracy: false })();
@@ -134,7 +145,7 @@ export function test(t) {
 
       t.it(
         'gets a result of the correct shape (with high accuracy), or ' +
-          'throws error if no permission (when trying again after 1 second)',
+          'throws error if no permission or disabled (when trying again after 1 second)',
         async () => {
           await new Promise(resolve => setTimeout(resolve, 1000));
           await testShapeOrUnauthorized({ enableHighAccuracy: true })();
