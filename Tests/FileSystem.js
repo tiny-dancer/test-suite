5f31ac80dbd36efec386a181e5e1c2d74fbecd55
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2017-06-22 23:45:42 +0000

    More `FileSystem` tests
    fbshipit-source-id: d9a5a3834

--- Tests/FileSystem.js
+++ Tests/FileSystem.js
@@ -5,10 +5,30 @@ export const name = 'FileSystem';
 import { NativeModules } from 'react-native';
 
 export function test(t) {
-  t.fdescribe('FileSystem', () => {
-    t.describe('ExponentFileSystem.downloadAsync()', () => {
-      t.it('downloads an image with correct MD5 hash, file name', async () => {
+  t.describe('FileSystem', () => {
+    t.it(
+      'delete(idempotent) -> !exists -> download(md5, uri) --> exists ' +
+        '--> delete --> !exists',
+      async () => {
         const filename = 'download1.png';
+
+        const assertExists = async expectedToExist => {
+          let { exists } = await NativeModules.ExponentFileSystem.getInfoAsync(
+            filename,
+            {}
+          );
+          if (expectedToExist) {
+            t.expect(exists).toBeTruthy();
+          } else {
+            t.expect(exists).not.toBeTruthy();
+          }
+        };
+
+        await NativeModules.ExponentFileSystem.deleteAsync(filename, {
+          idempotent: true,
+        });
+        await assertExists(false);
+
         const {
           md5,
           uri,
@@ -20,7 +40,27 @@ export function test(t) {
         t.expect(md5).toBe('1e02045c10b8f1145edc7c8375998f87');
         // NOTE: Is the below a sensible invariant to check?
         t.expect(uri.slice(-filename.length)).toBe(filename);
+        await assertExists(true);
+
+        await NativeModules.ExponentFileSystem.deleteAsync(filename, {});
+        await assertExists(false);
+      }
+    );
+
+    t.it('delete(idempotent) -> delete[error]', async () => {
+      const filename = 'willDelete.png';
+
+      await NativeModules.ExponentFileSystem.deleteAsync(filename, {
+        idempotent: true,
       });
+
+      let error;
+      try {
+        await NativeModules.ExponentFileSystem.deleteAsync(filename, {});
+      } catch (e) {
+        error = e;
+      }
+      t.expect(error.message).toMatch(/not.*found/);
     });
   });
 }
