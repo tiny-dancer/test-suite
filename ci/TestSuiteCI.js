84b1b4a5d4b1f0db216bd639c8299e6ab2e899b8
James Ide <ide@expo.io>
2017-08-17 02:30:55 +0000

    Make test-suite install expo-sdk using `yarn pack` as an intermediate step
    Using npm is causing CI to fail but the old code wasn't using the copy of expo-sdk in universe. `file:` URLs tend to behave differently than installing from npm, while packing simulates publishing and presumably installing from the pack should simulate installing from npm.
    
    So now we pack and install from the pack.
    
    fbshipit-source-id: 0cb1606

--- ci/TestSuiteCI.js
+++ ci/TestSuiteCI.js
@@ -4,30 +4,31 @@ import spawnAsync from '@expo/spawn-async';
 
 const universePath = Constants.CODE_UNIVERSE_DIR;
 const testSuitePath = path.join(universePath, 'apps', 'test-suite');
+const expoSdkPath = path.join(universePath, 'libraries', 'exponent-sdk');
+const expoSdkPackPath = path.join(testSuitePath, 'expo.tgz');
 
 export default {
   testSuitePath,
 
   async installNodeModulesAsync() {
-    // Use `npm` because `yarn` is being weird about using the latest local `exponent-sdk` package
-    Log.collapsed('Running `npm install` in test-suite');
-    await spawnAsync('npm', ['install'], {
+    Log.collapsed('Running `yarn install` in test-suite');
+    await spawnAsync('yarn', ['install'], {
       stdio: 'inherit',
       cwd: testSuitePath,
     });
 
-    // Install exponent-sdk from its code in the same commit.
+    // Pack exponent-sdk with its code in the same commit
+    Log.collapsed('Packing exponent-sdk');
+    await spawnAsync('yarn', ['pack', '--filename', expoSdkPackPath], {
+      stdio: 'inherit',
+      cwd: expoSdkPath,
+    });
+
+    // Install exponent-sdk from the packed package
     Log.collapsed('Installing exponent-sdk in test-suite');
-    await spawnAsync(
-      'npm',
-      [
-        'install',
-        `file://${path.join(universePath, 'libraries', 'exponent-sdk')}`,
-      ],
-      {
-        stdio: 'inherit',
-        cwd: testSuitePath,
-      }
-    );
+    await spawnAsync('yarn', ['add', `file:${expoSdkPackPath}`], {
+      stdio: 'inherit',
+      cwd: testSuitePath,
+    });
   },
 };
