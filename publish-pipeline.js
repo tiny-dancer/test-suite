5c209cf568e244ea4e55e95868cb71c7cc9a9015
Jesse Ruder <jesse@expo.io>
2017-08-22 23:17:16 +0000

    Cache test-suite
    fbshipit-source-id: 6687b3e

--- publish-pipeline.js
+++ publish-pipeline.js
@@ -1,10 +1,4 @@
-import { Log, XDL } from 'ci';
-
-import JsonFile from '@exponent/json-file';
-import spawnAsync from '@exponent/spawn-async';
-import path from 'path';
-
-import TestSuiteCI from './ci/TestSuiteCI';
+import { TestSuite } from 'ci';
 
 export default {
   config: {
@@ -24,18 +18,6 @@ const publishTestSuite = (branch, tag) => ({
     queue: 'builder',
   },
   async command() {
-    await spawnAsync('sysctl', ['-p']);
-
-    await TestSuiteCI.installNodeModulesAsync();
-
-    Log.collapsed('Modifying slug...');
-    let expJsonFile = new JsonFile(
-      path.join(TestSuiteCI.testSuitePath, 'exp.json')
-    );
-    let expJson = await expJsonFile.readAsync();
-    expJson.slug = `test-suite-${process.env.UNIVERSE_BUILD_ID}`;
-    await expJsonFile.writeAsync(expJson);
-
-    await XDL.publishProjectAsync(TestSuiteCI.testSuitePath);
+    await TestSuite.publishTestSuiteAsync();
   },
 });
