dd3793da332c828121499927b8c1a2bc915d1182
Craig Cronin <RoninSTi@users.noreply.github.com>
2017-09-09 04:32:55 +0000

    Fix SecureStore issues (#945)
    #This PR addresses the following issues:
    https://github.com/expo/universe/issues/889
    https://github.com/expo/universe/issues/884
    https://github.com/expo/universe/issues/878
    
    - The constants for `Expo.SecureStore.KeychainAccessible` enum weren't properly exported in the SDK.
    - Docs updated to more accurately reflect the `Expo.FileSystem.KeychainAccessible` enum.
    - When `Expo.SecureStore.getValueWithKeyAsync()` doesn't find a value, `<null>` should be returned instead of throwing
    - Android error messaging ambiguity addressed
    - Android encryption method changed from `RSA` to `AES` to allow for long string storage.
    - Long string stress case added to test suite.
    
    ## Backwards compatible:
    The changes to SecureStore are now backwards compatible, existing users who upgrade won't experience any data disruption.  Attempts to encrypt using a legacy `KeyStore.Entry` will generate a new entry and encrypt with AES.  Attempts to decrypt a legacy value that's never been re-encrypted with AES will be decrypted with legacy methods.
    
    fbshipit-source-id: b42aedc

--- tests/SecureStore.js
+++ tests/SecureStore.js
@@ -6,13 +6,15 @@ export const name = 'SecureStore';
 
 export function test(t) {
   const value = 'value-to-test';
+  const longValue =
+    'EAAT3TDAdWacBAMjZAq4clOJEOvf8JW5ZAxAsRnZCGRBNb1IRVFrzwiNqsM2I5MyogyPCc78TL1KZAFZAnZAFHeZCjkN8VMytKpcKD4HQEtVZBoAS54WkGbA2STjOe1vV3XOx3BY9OsDpDcD4yTZAv1OcI1wNlVvryiujZBeheVnELK6KTyzUgrPM8zZA42ZAB6SEcZADoj4MNsX5DqrJ3FtG0cxjFCD0lhKfBmTQMrZBCmuefRrQZDZDEAAT3TDAdWacBAMjZAq4clOJEOvf8JW5ZAxAsRnZCGRBNb1IRVFrzwiNqsM2I5MyogyPCc78TL1KZAFZAnZAFHeZCjkN8VMytKpcKD4HQEtVZBoAS54WkGbA2STjOe1vV3XOx3BY9OsDpDcD4yTZAv1OcI1wNlVvryiujZBeheVnELK6KTyzUgrPM8zZA42ZAB6SEcZADoj4MNsX5DqrJ3FtG0cxjFCD0lhKfBmTQMrZBCmuefRrQZDZDEAAT3TDAdWacBAMjZAq4clOJEOvf8JW5ZAxAsRnZCGRBNb1IRVFrzwiNqsM2I5MyogyPCc78TL1KZAFZAnZAFHeZCjkN8VMytKpcKD4HQEtVZBoAS54WkGbA2STjOe1vV3XOx3BY9OsDpDcD4yTZAv1OcI1wNlVvryiujZBeheVnELK6KTyzUgrPM8zZA42ZAB6SEcZADoj4MNsX5DqrJ3FtG0cxjFCD0lhKfBmTQMrZBCmuefRrQZDZDEAAT3TDAdWacBAMjZAq4clOJEOvf8JW5ZAxAsRnZCGRBNb1IRVFrzwiNqsM2I5MyogyPCc78TL1KZAFZAnZAFHeZCjkN8VMytKpcKD4HQEtVZBoAS54WkGbA2STjOe1vV3XOx3BY9OsDpDcD4yTZAv1OcI1wNlVvryiujZBeheVnELK6KTyzUgrPM8zZA42ZAB6SEcZADoj4MNsX5DqrJ3FtG0cxjFCD0lhKfBmTQMrZBCmuefRrQZDZD';
   const key = 'key-to-test';
   const emptyKey = null;
   const emptyValue = null;
   const options = { keychainService: 'test-service' };
   const optionsServiceA = { keychainService: 'service-A' };
   const optionsServiceB = { keychainService: 'service-B' };
-  t.describe('SecureStore: store -> fetch -> delete -> fetch -> err', () => {
+  t.describe('SecureStore: store -> fetch -> delete -> fetch -> err:', () => {
     t.it('Sets a value with a key', async done => {
       try {
         const result = await SecureStore.setValueWithKeyAsync(value, key, {});
@@ -50,15 +52,13 @@ export function test(t) {
     );
   });
   t.describe(
-    'SecureStore: store -> fetch -> delete -> fetch -> err with Options',
+    'SecureStore: store -> fetch -> delete -> fetch -> err with Options:',
     () => {
       t.it('Sets a value with a key and keychainService', async done => {
         try {
-          const result = await SecureStore.setValueWithKeyAsync(
-            value,
-            key,
-            options
-          );
+          const result = await SecureStore.setValueWithKeyAsync(value, key, {
+            keychainService: 'service',
+          });
           t.expect(result).toBe(undefined);
         } catch (e) {
           done.fail(e);
@@ -68,10 +68,9 @@ export function test(t) {
         'Fetch the value stored with the key and keychainService',
         async done => {
           try {
-            const fetchedValue = await SecureStore.getValueWithKeyAsync(
-              key,
-              options
-            );
+            const fetchedValue = await SecureStore.getValueWithKeyAsync(key, {
+              keychainService: 'service',
+            });
             t.expect(fetchedValue).toBe(value);
           } catch (e) {
             done.fail(e);
@@ -80,10 +79,9 @@ export function test(t) {
       );
       t.it('Delete the value associated with the key', async done => {
         try {
-          const result = await SecureStore.deleteValueWithKeyAsync(
-            key,
-            options
-          );
+          const result = await SecureStore.deleteValueWithKeyAsync(key, {
+            keychainService: 'service',
+          });
           t.expect(result).toBe(undefined);
         } catch (e) {
           done.fail(e);
@@ -93,10 +91,9 @@ export function test(t) {
         'Fetch the previously deleted key, expect no value error',
         async done => {
           try {
-            const fetchedValue = await SecureStore.getValueWithKeyAsync(
-              key,
-              options
-            );
+            const fetchedValue = await SecureStore.getValueWithKeyAsync(key, {
+              keychainService: 'service',
+            });
             done.fail(fetchedValue);
           } catch (e) {
             t.expect(e).toBeTruthy;
@@ -105,7 +102,7 @@ export function test(t) {
       );
     }
   );
-  t.describe('SecureStore: store with empty key -> err', () => {
+  t.describe('SecureStore: store with empty key -> err:', () => {
     t.it('Sets a value with an empty key, expect error', async done => {
       try {
         const result = await SecureStore.setValueWithKeyAsync(
@@ -119,7 +116,7 @@ export function test(t) {
       }
     });
   });
-  t.describe('SecureStore: store with empty Value -> err', () => {
+  t.describe('SecureStore: store with empty Value -> err:', () => {
     t.it('Sets an empty value with a key, expect error', async done => {
       try {
         const result = await SecureStore.setValueWithKeyAsync(
@@ -134,7 +131,7 @@ export function test(t) {
     });
   });
   t.describe(
-    'SecureStore: store value with keychainServiceA, fetch with keychainServiceB -> err',
+    'SecureStore: store value with keychainServiceA, fetch with keychainServiceB -> err:',
     () => {
       t.it('Sets a value with keychainServiceA', async done => {
         try {
@@ -157,6 +154,27 @@ export function test(t) {
           done.fail(result);
         } catch (e) {
           t.expect(e).toBeTruthy;
+        }
+      });
+    }
+  );
+  t.describe(
+    'SecureStore: store long value, fetch long value -> Success:',
+    () => {
+      t.it('Set long value', async done => {
+        try {
+          const result = await SecureStore.setValueWithKeyAsync(longValue, key);
+          t.expect(result).toBe(undefined);
+        } catch (e) {
+          done.fail(e);
+        }
+      });
+      t.it('Fetch long value', async done => {
+        try {
+          const result = await SecureStore.getValueWithKeyAsync(key);
+          t.expect(result).toBe(longValue);
+        } catch (e) {
+          done.fail(e);
         }
       });
     }
