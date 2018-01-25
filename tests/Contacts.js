d79fbfebfd6dbcfb1c9c41fc5a4c6304ccbd8157
tiny-dancer <grose.matthewf@gmail.com>
2018-01-24 23:01:19 -0600

    initial contacts updates

--- tests/Contacts.js
+++ tests/Contacts.js
@@ -6,7 +6,7 @@ import * as TestUtils from '../TestUtils';
 export const name = 'Contacts';
 
 export function test(t) {
-  t.describe('Contacts', () => {
+  t.fdescribe('Contacts', () => {
     let firstContact;
     t.describe('Contacts.getContactsAsync()', () => {
       t.it('gets permission and at least one result, all results of right shape', async () => {
@@ -50,6 +50,58 @@ export function test(t) {
         });
       });
 
+      t.it('skips additional properties if fields is empty', async () => {
+        const contacts = await Contacts.getContactsAsync({
+          fields: [],
+        });
+        t.expect(contacts.total > 0).toBe(true);
+        t.expect(contacts.data.length > 0).toBe(true);
+        contacts.data.forEach(contact => {
+          const toSkip = [
+            phoneNumbers,
+            emails,
+            addresses,
+            socialProfiles,
+            instantMessageAddresses,
+            urls,
+            dates,
+            relationships,
+            note,
+            namePrefix,
+            nameSuffix,
+            phoneticFirstName,
+            phoneticMiddleName,
+            phoneticLastName,
+          ];
+          toSkip.forEach(entry => {
+            t.expect(typeof entry === 'undefined').toBe(true);
+          });
+        });
+      });
+
+      t.it('returns consistent image data', async () => {
+        const contacts = await Contacts.getContactsAsync({
+          fields: [Contacts.IMAGE, Contacts.THUMBNAIL],
+        });
+        if (contacts.total > 0) {
+          contacts.data.forEach(contact => {
+            if (contact.imageAvailable) {
+              t.expect(typeof contact.thumbnail === 'object').toBe(true);
+              t.expect(typeof contact.thumbnail.uri === 'string').toBe(true);
+              t.expect(typeof contact.image === 'object' || typeof contact.image === 'undefined');
+              if (contact.image) {
+                t.expect(typeof contact.image.uri === 'string').toBe(true);
+              }
+            } else {
+              t.expect(contact.thumbnail.uri).toBe(null);
+              if (contact.image) {
+                t.expect(contact.image.uri).toBe(null);
+              }
+            }
+          });
+        }
+      });
+
       t.it('respects the page size', async () => {
         const contacts = await Contacts.getContactsAsync({
           fields: [Contacts.PHONE_NUMBERS],
@@ -67,11 +119,13 @@ export function test(t) {
           pageOffset: 0,
           pageSize: 2,
         });
+        console.log(`FIRST PAGE: ${JSON.stringify(firstPage)}`);
         const secondPage = await Contacts.getContactsAsync({
           fields: [Contacts.PHONE_NUMBERS],
           pageOffset: 1,
           pageSize: 2,
         });
+        console.log(`SECOND PAGE: ${JSON.stringify(secondPage)}`);
 
         if (firstPage.total >= 3) {
           t.expect(firstPage.data.length).toBe(2);
@@ -86,14 +140,112 @@ export function test(t) {
       t.it('gets a result of right shape', async () => {
         if (firstContact) {
           const contact = await Contacts.getContactByIdAsync({
-            fields: [Contacts.PHONE_NUMBERS, Contacts.EMAILS],
+            fields: [
+              Contacts.PHONE_NUMBERS,
+              Contacts.EMAILS,
+              Contacts.ADDRESSES,
+              Contacts.NOTE,
+              Contacts.BIRTHDAY,
+              Contacts.NON_GREGORIAN_BIRTHDAY,
+              Contacts.NAME_PREFIX,
+              Contacts.NAME_SUFFIX,
+              Contacts.PHONETIC_FIRST_NAME,
+              Contacts.PHONETIC_MIDDLE_NAME,
+              Contacts.PHONETIC_LAST_NAME,
+              Contacts.SOCIAL_PROFILES,
+              Contacts.IM_ADDRESSES,
+              Contacts.URLS,
+              Contacts.DATES,
+              Contacts.RELATIONSHIPS,
+            ],
             id: firstContact.id,
           });
-          const { id, name, phoneNumbers, emails } = contact;
+          const {
+            id,
+            name,
+            firstName,
+            middleName,
+            lastName,
+            nickname,
+            jobTitle,
+            company,
+            department,
+            imageAvailable,
+            previousLastName, // ios only
+            phoneNumbers,
+            emails,
+            addresses,
+            image,
+            thumbnail,
+            note,
+            nonGregorianBirthday, // ios only
+            namePrefix,
+            nameSuffix,
+            phoneticFirstName,
+            phoneticMiddleName,
+            phoneticLastName,
+            socialProfiles, // ios only
+            instantMessageAddresses,
+            urls,
+            dates,
+            relationships,
+            birthday,
+          } = contact;
+
           t.expect(typeof id === 'string' || typeof id === 'number').toBe(true);
           t.expect(typeof name === 'string' || typeof name === 'undefined').toBe(true);
           t.expect(Array.isArray(phoneNumbers) || typeof phoneNumbers === 'undefined').toBe(true);
           t.expect(Array.isArray(emails) || typeof emails === 'undefined').toBe(true);
+
+          t.expect(typeof id === 'string' || typeof id === 'number').toBe(true);
+          t.expect(typeof name === 'string' || typeof name === 'undefined').toBe(true);
+          t.expect(Array.isArray(phoneNumbers) || typeof phoneNumbers === 'undefined').toBe(true);
+          t.expect(Array.isArray(emails) || typeof emails === 'undefined').toBe(true);
+          t
+            .expect(typeof imageAvailable === 'boolean' || typeof imageAvailable === 'undefined')
+            .toBe(true);
+          const strings = [
+            name,
+            firstName,
+            middleName,
+            lastName,
+            previousLastName,
+            nickname,
+            company,
+            jobTitle,
+            department,
+            note,
+            namePrefix,
+            nameSuffix,
+            phoneticFirstName,
+            phoneticMiddleName,
+            phoneticLastName,
+          ];
+          strings.forEach(string => {
+            t.expect(typeof string === 'string' || typeof string === 'undefined').toBe(true);
+          });
+
+          const arrays = [
+            phoneNumbers,
+            emails,
+            addresses,
+            socialProfiles,
+            instantMessageAddresses,
+            urls,
+            dates,
+            relationships,
+          ];
+          arrays.forEach(array => {
+            t.expect(Array.isArray(array) || typeof array === 'undefined').toBe(true);
+          });
+
+          t.expect(typeof birthday === 'object' || typeof birthday === 'undefined').toBe(true);
+          t
+            .expect(
+              typeof nonGregorianBirthday === 'object' ||
+                typeof nonGregorianBirthday === 'undefined'
+            )
+            .toBe(true);
         }
       });
     });
