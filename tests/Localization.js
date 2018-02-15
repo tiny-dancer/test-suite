6a8f3a663ea47669f8aa79577fcae5c23f7f78c6
Ben Roth <spongebucket@gmail.com>
2018-02-15 01:02:43 +0000

    Cover all Localization methods
    fbshipit-source-id: 8b2b600

--- tests/Localization.js
+++ tests/Localization.js
@@ -5,18 +5,24 @@ import { Localization } from 'expo';
 export const name = 'Localization';
 
 export function test(t) {
-  t.describe(`Test Localization Functions`, () => {
-    t.it('Gets the current device country correctly', async () => {
+  t.fdescribe(`Localization does something`, () => {
+    t.it('Gets the current device country', async () => {
       const result = await Localization.getCurrentDeviceCountryAsync();
       t.expect(result).not.toBe(undefined);
     });
-    t.it('Gets the current locale correctly', async () => {
+    t.it('Gets the current locale', async () => {
       const result = await Localization.getCurrentLocaleAsync();
       t.expect(result).not.toBe(undefined);
     });
-    // TODO: getPreferredLocales
-    // TODO: getISOCurrencyCodes
-    t.it('Gets the current timzezone correctly', async () => {
+    t.it('Gets the preferred locales', async () => {
+      const result = await Localization.getPreferredLocalesAsync();
+      t.expect(result).not.toBe(undefined);
+    });
+    t.it('Gets ISO currency codes', async () => {
+      const result = await Localization.getISOCurrencyCodesAsync();
+      t.expect(result).not.toBe(undefined);
+    });
+    t.it('Gets the current timzezone', async () => {
       const result = await Localization.getCurrentTimeZoneAsync();
       t.expect(result).not.toBe(undefined);
     });
