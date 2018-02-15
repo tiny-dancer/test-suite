208e0caa3ff0195cdba62e45d51a6df2a1f4749f
Ben Roth <spongebucket@gmail.com>
2018-02-15 00:48:54 +0000

    Move Util tests to Localization, fix localization style
    fbshipit-source-id: d9e0a63

--- tests/Localization.js
+++ tests/Localization.js
@@ -1,21 +1,23 @@
 'use strict';
 
-import { Util } from 'expo';
+import { Localization } from 'expo';
 
-export const name = 'Util';
+export const name = 'Localization';
 
 export function test(t) {
-  t.describe(`Test Util Functions`, () => {
+  t.describe(`Test Localization Functions`, () => {
     t.it('Gets the current device country correctly', async () => {
-      const result = await Util.getCurrentDeviceCountryAsync();
+      const result = await Localization.getCurrentDeviceCountryAsync();
       t.expect(result).not.toBe(undefined);
     });
     t.it('Gets the current locale correctly', async () => {
-      const result = await Util.getCurrentLocaleAsync();
+      const result = await Localization.getCurrentLocaleAsync();
       t.expect(result).not.toBe(undefined);
     });
+    // TODO: getPreferredLocales
+    // TODO: getISOCurrencyCodes
     t.it('Gets the current timzezone correctly', async () => {
-      const result = await Util.getCurrentTimeZoneAsync();
+      const result = await Localization.getCurrentTimeZoneAsync();
       t.expect(result).not.toBe(undefined);
     });
   });
